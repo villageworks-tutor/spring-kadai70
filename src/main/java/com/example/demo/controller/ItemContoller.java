e361b8463175332d15aa7f8a0c175204651650b1
villageworks <villageworks@gmail.com>
2023-12-04 06:14:44 +0900

    ver.0.32: 【shallowNestingCode】条件の入れ子構造を浅くした組合せによる検索

--- src/main/java/com/example/demo/controller/ItemContoller.java
+++ src/main/java/com/example/demo/controller/ItemContoller.java
@@ -36,30 +36,21 @@ public class ItemContoller {
 		// リクエストパラメータの有無によって商品リストの取得を分岐
 		// TODO: この商品リスト取得はオブジェクト指向の立場からサービスとして分割するのが適切
 		List<Item> itemList = null;
-		if (!keyword.isEmpty()) {
-			// キーワードが入力されている場合
-			if (maxPrice != null) {
-				// 価格検索上限が入力されている場合：キーワード検索かつ価格上限値以下検索
-				itemList = itemRepository.findByNameContainingAndPriceLessThanEqual(keyword, maxPrice);
-			} else {
-				// 価格検索上限が入力されていない場合：キーワード検索（Step1のコーディングに対応）
-				itemList = itemRepository.findByNameContaining(keyword);
-			}
+		if (!keyword.isEmpty() && maxPrice != null) {
+			// キーワードが入力されかつ価格検索上限値が入力されている場合：キーワード検索かつ価格上限値検索
+			itemList = itemRepository.findByNameContainingAndPriceLessThanEqual(keyword, maxPrice);
+		} else if (!keyword.isEmpty() && maxPrice == null) {
+			// キーワードが入力されかつ価格上限値が入力されていない場合：キーワード検索
+			itemList = itemRepository.findByNameContaining(keyword);
+		} else if (keyword.isEmpty() && maxPrice != null) {
+			// キーワードが入力されずかつ価格上限値が入力されている場合：価格上限値以下検索
+			itemList = itemRepository.findByPriceLessThanEqual(maxPrice);
+		} else if (keyword.isEmpty() && maxPrice == null && categoryId != null) {
+			// キーワードも価格上限値も入力されていない場合でかつカテゴリーのリンクが押下された場合：カテゴリ検索
+			itemList = itemRepository.findByCategoryId(categoryId);
 		} else {
-			// キーワードが入力されていない場合
-			if (maxPrice != null) {
-				// 価格検索上限が入力されている場合：価格上限値以下検索
-				itemList = itemRepository.findByPriceLessThanEqual(maxPrice);
-			} else {
-				// 価格検索上限が入力されていない場合
-				if (categoryId != null) {
-					// カテゴリーのリンクが押下された場合：カテゴリー検索
-					itemList = itemRepository.findByCategoryId(categoryId);
-				} else {
-					// カテゴリーのリンクが押下されていない場合：全件検索（初期表示またはサイトタイトルのリンク押下時のコーディングに対応）
-					itemList = itemRepository.findAll();
-				}
-			}
+			// 上記以外の場合（初期表示およびサイト名のリンクが押下された場合）：全検索
+			itemList = itemRepository.findAll();
 		}
 		
 		// 取得したカテゴリーリストと取得した商品をスコープに登録
